be95920eb3bb25db98d3ecd7280470d8acbdc9d7
Morad Oulhaj <159066460+moradoulhaj@users.noreply.github.com>
2024-11-26 11:21:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/merger/',  // Replace 'your-repo-name' with the actual name of your GitHub repository
+  base: '/merger/',  
   plugins: [react()],
 })
