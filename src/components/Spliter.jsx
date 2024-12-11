fe5238378df4b2e580b1dc70ca2847c10425d514
moradoulhaj <moradoulhaj.off@gmail.com>
2024-12-11 10:27:54 +0000

    updates

--- src/components/Spliter.jsx
+++ src/components/Spliter.jsx
@@ -1,4 +1,4 @@
-import React, { useState , lazy } from "react";
+import React, { useState  } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { RotateCcw } from "lucide-react";
